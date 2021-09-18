ac1d5c2f97b99b3ed8367abe0b2b34d29036676d
spencerraines88 <89616099+spencerraines88@users.noreply.github.com>
2021-09-17 19:20:48 -0500

    Update hwexpressions.cpp

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -7,7 +7,15 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+//functions
+double get_sales_tax_amount(double meal_amount)
+{
+    //get product
+    return meal_amount * (TAX_RATE/100);
+}
 
-
-
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    //get product
+    return meal_amount * (tip_rate/100);
+}
