2dd5e8f93f02723ed7a1e69f407fbf8f078a4c0f
spencerraines88 <89616099+spencerraines88@users.noreply.github.com>
2021-10-22 20:55:18 -0500

    Update main.cpp

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,45 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+#include "func.h"
+#include <string>
+//write using statements
+using std::cout;using std::cin;using std::string;using namespace std;
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
 
-*/
 int main() 
 {
+	int choice;
+	string dna;
+	char varification;
+	bool vars = true;
+	do{
+		
+		cout<<"1- Get GC Content\n";
+		cout<<"2- Get DNA Complement\n";
+		cout<<"3-Exit\n";
+		cin>>choice;
+		if(choice == 1)
+		{
+			cout<<"Enter DNA Sequence: ";
+			cin>>dna;
+			cout<<"\nGC Content: "<< get_gc_content(dna)<<"\n";
+		}
+		if(choice == 2)
+		{
+			cout<<"Enter DNA Sequence: ";
+			cin>>dna;
+			cout<<"\nDNA Complement: "<< get_dna_complement(dna)<<"\n";
+		}
+		if(choice == 3)
+		{
+			cout<<"Are you sure you want to exit? (y/other char)\n";
+			cin>>ws>>varification;
+			if(varification == 'Y'|| varification  == 'y')
+			{
+				vars = false;
+			}else{
+				vars = true;
+			}
+		}
 
+	}while(vars);
 	return 0;
 }
